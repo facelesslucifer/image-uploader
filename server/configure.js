aab1632d10f0c389d59ba2a4710a6c014b50b5b5
Ye Lin Htun <yelinhtun1987@gmail.com>
2016-01-18 16:58:33 +0800

    Add JS script for image page

--- server/configure.js
+++ server/configure.js
@@ -6,16 +6,12 @@ var path = require('path'),
     cookieParser = require('cookie-parser'),
     morgan = require('morgan'),
     methodOverride = require('method-override'),
-    errorHandler = require('errorhandler');
-    moment = require('moment');
-    multer = require('multer');
+    errorHandler = require('errorhandler'),
+    moment = require('moment'),
+    multer = require('multer'),
+    upload = multer({dest: path.join(__dirname, 'public/upload/temp')});
 
 module.exports = function(app) {
-    app.use(morgan('dev'));
-    app.use(bodyParser.urlencoded({'extended': true}));
-    app.use(multer({
-        dest: path.join(__dirname, 'public/upload/temp')
-    }));
     app.engine('handlebars', exphbs.create({
         defaultLayout: 'main',
         layoutsDir: app.get('views') + '/layouts',
@@ -28,14 +24,20 @@ module.exports = function(app) {
         }
     }).engine);
     app.set('view engine', 'handlebars');
+
+    app.use(morgan('dev'));
+    // app.use(multer({ dest: path.join(__dirname, 'public/upload/temp')}));
+
     app.use(methodOverride());
     app.use(cookieParser('some-secret-value-here'));
     routes(app);
 
     app.use('/public/', express.static(path.join(__dirname, '../public')));
-    if('development' === app.get('env')) {
-        app.use(errorHandler());
+
+    if ('development' === app.get('env')) {
+       app.use(errorHandler());
     }
 
     return app;
 };
+
